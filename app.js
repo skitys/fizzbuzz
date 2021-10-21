9564f99cd455d21ee7aa450860cc6c46ea3f43bb
skitys <ts.sakai.seraku@gmail.com>
2021-10-21 20:06:09 +0900

    update fizzbuzz

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const server = http.createServer((req,res) => {
     }
     res.writeHead(200,{'Content-Type': 'text/html'})
     res.write(data)
-    res.end('Hello World')
+    res.end()
   })
 })
 
