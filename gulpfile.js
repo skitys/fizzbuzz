032a0bba967dfade8ca6c448a0a1c0b66ceaf9b1
skitys <ts.sakai.seraku@gmail.com>
2021-10-22 16:45:50 +0900

    add sass

--- gulpfile.js
+++ gulpfile.js
@@ -4,18 +4,38 @@ const ejs = require('gulp-ejs')
 const rename = require('gulp-rename')
 const plumber = require('gulp-plumber')
 const notify = require('gulp-notify')
-/* HTML */
+const g_sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer')
+const sourcemap = require('gulp-sourcemaps')
+/* common */
 const path = {
   src: 'src',
   dist: 'dist'
 }
+/* HTML */
+const json = JSON.parse(fs.readFileSync(path.src + '/_data/_data.json'))
 gulp.task('ejs', ()=>{
   return gulp.src([
     path.src + '/ejs/**/' + '*.ejs',
     '!' + path.src + '/ejs/**/' + '_*.ejs'
   ]).pipe(plumber())
-  .pipe(ejs(/*{ json: json}, {},{}*/))
+  .pipe(ejs({ json: json}, {},{}))
   .pipe(rename({extname: '.html'}))
   .pipe(gulp.dest(path.dist))
 })
-gulp.task('default', gulp.series('ejs'))
+/* CSS */
+gulp.task('style', (done)=>{
+  gulp.src(path.src + '/sass/**/' + '*.scss')
+  .pipe(sourcemap.init({ largeFile: true }))
+  .pipe(g_sass({
+    outputStyle: 'expanded'
+  }))
+  .pipe(autoprefixer({
+    cascade: false
+  }))
+  .pipe(gulp.dest(path.dist))
+  .pipe(sourcemap.write('_maps'))
+  .pipe(gulp.dest(path.dist))
+  done();
+})
+gulp.task('default', gulp.series('ejs', 'style'))
